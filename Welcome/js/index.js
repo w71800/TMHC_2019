474795c6bb7f449957d2c66754b5c951602e9fe0
IKaRos <w71800@gmail.com>
2019-05-08 16:35:45 +0800

    大致完成版面

--- Welcome/js/index.js
+++ Welcome/js/index.js
@@ -10,13 +10,22 @@ $("section").click(function()
     $("#navi_bar").addClass("navi_off");
   })
 
-// $(".list_fold").click(function()
-//   {
-//     $(this).removeClass("list_fold")
-//     $(this).addClass("list_expand")
-//   })
-// $(".list_expand").click(function()
-//   {
-//     $(this).removeClass("list_expand")
-//     $(this).addClass("list_fold")
-//   })
+$(".switcher_bus").click(function()
+  {
+    $(this).addClass("hint_border")
+    $(this).css("background-color","white")
+    $(".bus").removeClass("invisible")
+    $(".mrt").addClass("invisible") 
+    $(".switcher_mrt").removeClass("hint_border")
+    $(".switcher_mrt").css("background-color","#F1F3F3")
+  })
+
+$(".switcher_mrt").click(function()
+  {
+    $(this).addClass("hint_border")
+    $(this).css("background-color","white")
+    $(".mrt").removeClass("invisible")
+    $(".bus").addClass("invisible")
+    $(".switcher_bus").removeClass("hint_border")
+    $(".switcher_bus").css("background-color","#F1F3F3")
+  })
